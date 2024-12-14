f117f8bbbf488181bea458bcb438629daf95bba0
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-12-14 11:39:40 -0300

    feat: dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LinkController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\EnsureUserIsAdmin;
@@ -9,18 +10,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('links', LinkController::class)
         ->only(['index', 'store', 'edit', 'update', 'destroy'])
-        ->middleware(['verified', EnsureUserIsAdmin::class]);
+        ->middleware(EnsureUserIsAdmin::class);
 });
 
 require __DIR__.'/auth.php';
